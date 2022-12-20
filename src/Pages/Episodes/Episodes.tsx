e89abc7f987d7654230e5d9e1a3b8c8d6c1b23c0
kamser <keylor.morataya@gmail.com>
2022-12-19 23:10:25 -0600

    Added the basic structure for the container

--- src/Pages/Episodes/Episodes.tsx
+++ src/Pages/Episodes/Episodes.tsx
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import EpisodesFilter from '../../Components/episodesFilter';
+import EpisodeSectionContainer from '../../Components/episodeSectionContainer';
 
 
 const Episodes = () => {
@@ -13,6 +14,7 @@ const Episodes = () => {
                 <EpisodesFilter></EpisodesFilter>
                 <h1>This is the Episodes Page</h1>
             </div>
+            <EpisodeSectionContainer></EpisodeSectionContainer>
         </>
     );
 
